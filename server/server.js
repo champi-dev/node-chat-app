a3e54a41d385e11f475519fa527770ddf536e92c
devsarmico <danielsarcor@gmail.com>
2019-01-15 19:15:48 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,18 +14,30 @@ const io = socketIO(server)
 io.on('connection', (socket) => {
   console.log('New user connected')
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: Date.now()
+  })
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: Date.now()
+  })
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message)
-    // io.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // })
-    socket.broadcast.emit('newMessage', {
+    io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     })
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: Date.now()
+    // })
   })
 
   socket.on('disconnect', () => {
